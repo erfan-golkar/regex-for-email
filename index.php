7b7c5fa6a91655ce1192602bca6974a190e15eee
Erfan Golkar <mr.golkaar@gmail.com>
2023-06-20 12:26:04 +0330

    add style

--- index.php
+++ index.php
@@ -4,8 +4,15 @@
   <title>regEx for email</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  
+  <form action="index.php" method="post">
+    <fieldset>
+      <legend>regEx for Email</legend>
+      <input name="email" type="email" placeholder="Your Email">
+      <button type="submit">Submit</button>
+    </fieldset>
+  </form>
 </body>
 </html>
