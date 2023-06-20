ac3ad3f0f8f76083f88c36d0e345bbc25eb366ce
Erfan Golkar <mr.golkaar@gmail.com>
2023-06-20 13:14:12 +0330

    email validation

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+if (isset($_POST["email"]) && $_POST["email"] !== "") {
+    $pattern = "/^[\w\-\.]+@{1}[A-Za-z]+\.{1}[A-Za-z]{2,6}$/";
+    $is_match = preg_match($pattern, $_POST["email"]);
+    if ($is_match === 1) {
+      ?><script>alert("Valid Email!")</script><?php
+    } else {
+    ?><script>alert("Invalid Email!")</script><?php
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
